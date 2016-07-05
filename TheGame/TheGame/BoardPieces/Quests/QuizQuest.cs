63e3027de8b70a44a0d3f85a8ff1d7c32046195a
alexizvely <alexizvely@gmail.com>
2016-07-05 12:22:30 +0300

    added some questions

--- TheGame/TheGame/BoardPieces/Quests/QuizQuest.cs
+++ TheGame/TheGame/BoardPieces/Quests/QuizQuest.cs
@@ -12,6 +12,20 @@
         private int id;
         private string displaySymbol = Constants.QuestDisplaySymbol;
         private ConsoleColor displayColor = ConsoleColor.DarkGreen;
+        private Dictionary<string, string> quizes = new Dictionary<string, string>
+        {
+            { "When did Bulgaria join the EU?", "2007" },
+            {"Porsche is a brand of car that originated in what country?", "Germany" },
+            {"Who was the first human to travel into space?", "Yuri Gagarin" },
+            {"The Roman numeral \"D\" stands for what number?", "500" },
+            {"What is the name for the Greek goddess of victory?", "Nike" },
+            {"According to Greek mythology which Gorgon had snakes for hair and could turn onlookers into stone?", "Medusa" },
+            {"The Roman numeral \"L\" stands for what number?", "50" },
+            {"In what year did the Titanic sink?","1912" },
+            {"According to Greek mythology, who was the goddess of beauty?","Aphrodite" },
+            {"Who was the first woman pilot to fly solo across the Atlantic?","Amelia Earhart" }
+
+        };
 
         public QuizQuest(List<Position> initialPositions)
         {
